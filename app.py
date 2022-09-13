aeaad875238c1d3a557230b133b511dd0c02c49d
Eddie <president@cybersoc.org.uk>
2022-09-13 23:04:14 +0100

    Add base bootstrap page and add_to_navbar decorator

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+navbar_page_names = dict()
+
+
+@app.context_processor
+def define_name_constants() -> dict:
+    """
+    We'll define some name constants here in case we want to change them in the future.
+    The `context_processor` decorator means they're accessible to all templates/pages by default.
+    They're used like any other template variable, like {{ company_name }}.
+
+    :rtype: dict
+    :return: a dictionary of name constants
+    """
+    return dict(company_name="Falihax",
+                navbar_page_names=navbar_page_names)
+
+
+def add_to_navbar(name: str):
+    """
+    A decorator to add a page to the navbar. You don't need to edit this.
+    """
+    def __inner(f):
+        global navbar_page_names
+        navbar_page_names[name] = f
+        return f
+    return __inner
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@add_to_navbar("Home")
+def homepage():
+    return render_template("base.html", title="Homepage")
+
 
 if __name__ == '__main__':
     # run this code on app start
